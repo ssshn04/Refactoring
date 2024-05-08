c480f7345c5577f30fdcbacb0233ac77441db420
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-08 23:24:45 +0300

    lab 6 task 1.3 commit

---
+++ src/main/java/lab6/task1_3/Employee.java
@@ -0,0 +1,15 @@
+package lab6.task1_3;
+
+public class Employee {
+    private String name;
+    private String department;
+    private double salary;
+    // Конструктор та інші методи класу...
+    public void printEmployeeDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+    // Інші методи класу...
+}
+
