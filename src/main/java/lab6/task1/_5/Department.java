6aedf8f6d2372367631251f0634aae2731a2d937
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-13 20:01:30 +0300

    lab 6 task 1.5 before refactoring

---
+++ src/main/java/lab6/task1/_5/Department.java
@@ -0,0 +1,10 @@
+package lab6.task1._5;
+
+public class Department {
+    private String name;
+    private Employee manager;
+    // Конструктор та інші методи класу...
+    public Employee getManager() {
+        return manager;
+    }
+}
---
+++ src/main/java/lab6/task1/_5/Employee.java
@@ -0,0 +1,9 @@
+package lab6.task1._5;
+
+public class Employee {
+    private String name;
+    private Department department;
+    public Department getDepartment() {
+        return department;
+    }
+}
