2e6f885655d10c4436dfd23583d54856ad8b2534
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-14 11:24:09 +0300

    lab 6 task 5.4  was refactored

---
+++ src/main/java/lab6/task5/_4/Animal.java
@@ -0,0 +1,15 @@
+package lab6.task5._4;
+class Animal {
+
+}
+
+class Dog extends Animal {
+
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
---
+++ src/main/java/lab6/task5/_4/Main.java
@@ -0,0 +1,8 @@
+package lab6.task5._4;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.makeSound();
+    }
+}
