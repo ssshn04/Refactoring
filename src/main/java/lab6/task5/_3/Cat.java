ecc435f0ec5bcc761d25ac58b1a3670c95d71b54
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-14 11:22:24 +0300

    lab 6 task 5.3  was refactored

---
+++ src/main/java/lab6/task5/_3/Cat.java
@@ -0,0 +1,10 @@
+package lab6.task5._3;
+
+public class Cat extends Animal {
+    String color;
+
+    public Cat(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+}
---
+++ src/main/java/lab6/task5/_3/Dog.java
@@ -0,0 +1,10 @@
+package lab6.task5._3;
+
+public class Dog extends Animal {
+    String breed;
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+}
