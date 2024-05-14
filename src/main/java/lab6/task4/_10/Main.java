2de5be3b1740e476dd02e20d2e5ff136e006397c
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-14 10:55:14 +0300

    lab 6 task 4.10  was refactored

---
+++ src/main/java/lab6/task4/_10/Main.java
@@ -0,0 +1,10 @@
+package lab6.task4._10;
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Vicky", 18);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
+
