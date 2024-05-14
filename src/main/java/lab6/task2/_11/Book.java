71516ae9003b75b366a6c72c23a4761848d62509
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-05-14 09:59:14 +0300

    lab 6 task 2.11  was refactored

---
+++ src/main/java/lab6/task2/_11/Book.java
@@ -0,0 +1,44 @@
+package lab6.task2._11;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
+
+        System.out.println("Book 1: " + book1);
+        System.out.println("Book 2: " + book2);
+    }
+}
+
---
+++ src/main/java/lab6/task2/_11/Library.java
@@ -0,0 +1,26 @@
+package lab6.task2._11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library(List<Book> books) {
+        this.books = new ArrayList<>(books);
+    }
+
+    public List<Book> getBooks() {
+        return Collections.unmodifiableList(books);
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+}
+
